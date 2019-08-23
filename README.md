ce3a83f65d7ac963b659c28af974161411bb248e
leyla li <leylali@qq.com>
2019-08-23 14:38:27 -0700

    merge conflicts

---
+++ README.md
@@ -0,0 +1,37 @@
+# LAB-09
+
+## Express Routing and Connected API
+
+### Author: Leyla Li
+
+### Links and Resources
+* [submission PR](https://github.com/401-advanced-javascript-leyla/lab-09/pulls)
+* [travis](https://www.travis-ci.com/search/lab-09)
+
+#### Documentation
+* [jsdoc](http://localhost:3000/api/v1/docs)
+* [jsdoc](http://localhost:5000/api-docs)
+
+
+### Modules
+#### `mongo.js`
+#### `products.js`
+#### `categories.js`
+
+### Setup
+* `PORT` - 3000
+* `SWAGGER PORT` - 5000
+* `MONGODB_URI` - inclueded in index.js
+
+#### Running the app
+* `npm start`
+* Endpoint: `/api/v1/categories`
+  * Returns a JSON object with categories data in it.
+* Endpoint: `/api/v1/products`
+  * Returns a JSON object with products in it.
+  
+#### Tests
+* `npm run test`
+
+#### UML
+Link to an image of the UML for your application and response to events
---
+++ src/app.js
@@ -0,0 +1,46 @@
+'use strict';
+
+// 3rd Party Resources
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const swagger = require('./api/swagger');
+
+const apiRouter = require('../routes/api');
+
+// Esoteric Resources
+const errorHandler = require( './middleware/error.js');
+const notFound = require( './middleware/404.js' );
+
+// Prepare the express app
+const app = express();
+
+// App Level MW
+app.use(cors());
+app.use(morgan('dev'));
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+//jsdoc
+app.use('/docs', express.static('./docs'));
+
+// Routes
+app.use(apiRouter);
+
+// Catchalls
+app.use('/*',notFound);
+app.use(errorHandler);
+
+
+
+// app.listen(3000, () => console.log(`Server up on port 3000`) );
+module.exports = {
+  server: app,
+  start: port=>{
+    const PORT = port || 3000;
+    app.listen(PORT, ()=>{
+      console.log(`Server is up at ${PORT}`);
+    });
+  },
+};
